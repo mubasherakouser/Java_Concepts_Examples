f8df096aa3ea71db0e324d916db455bd3d33d5ac
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 14:51:02 +0530

    Default_Constructor.java

---
+++ Default_Constructor.java
@@ -0,0 +1,16 @@
+//Default Constructor
+package com.jspiders.constructors;
+class Demo{
+	int n;
+	double d;
+	char p;
+}
+public class Mainclass {
+	public static void main(String[] args) {
+		Demo ref=new Demo();
+		System.out.println(ref.n);
+		System.out.println(ref.d);
+		System.out.println(ref.p);
+	}
+
+}
