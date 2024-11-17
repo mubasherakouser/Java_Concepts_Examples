45e9f8e9776d0220653f33c3b20b447480388cbb
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 15:19:35 +0530

    Multilevel_Inheritance.java

---
+++ Multilevel_Inheritance.java
@@ -0,0 +1,30 @@
+//Multilevel Inheritance
+package com.jspiders.inheritance;
+class A{
+	void test() {
+		System.out.println("Test().....");
+	}
+}
+class B extends A{
+	void disp() {
+		System.out.println("disp()......");
+	}
+}
+class C extends B{
+	void play() {
+		System.out.println("play()......");
+	}
+}
+public class Mainclass {
+	public static void main(String[] args) {
+		A a1=new A();
+		a1.test();
+		B b1=new B();
+		b1.test();
+		b1.test();
+		C c1=new C();
+		c1.test();
+		c1.disp();
+		c1.play();
+	}
+}
