77d01bf654f7ecb0a8d0f8111a73fcc79d1a38c7
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 15:38:42 +0530

    Narrowing_Typecasting.java

---
+++ Narrowing_Typecasting.java
@@ -0,0 +1,15 @@
+//Narrowing Typecasting
+package com.Typecasting;
+public class Mainclass {
+	public static void main(String[] args) {
+		float a=4.5f;
+		int b=(int)a;//Narrowing :float---->int
+		System.out.println(b);
+		int x=97;
+		char y=(char)x;
+		System.out.println(y);
+		long p=3456789L;
+		int q=(int)p;
+		System.out.println(q);
+	}
+}
