2a9cffb4acf87be3d1613ac27cde2125da992cd0
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 15:33:04 +0530

    Widening_Typecasting.java

---
+++ Widening_Typecasting.java
@@ -0,0 +1,15 @@
+//Widening Typecasting
+package com.jspiders.Typecasting;
+public class Mainclass {
+	public static void main(String[] args) {
+		int a=10;
+		float b=a;//int-->float
+		System.out.println(b);
+		long v=12345L;
+		double d=v;//long-->double
+		System.out.println(d);
+		char c='A';
+		int x=c;//char-->int
+		System.out.println(x);
+	}
+}
